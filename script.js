98fa97dc61814bacde5660298d97e40c6744a566
BaconDeGelo <pedro.ugo2015@gmail.com>
2025-08-18 21:00:31 -0300

    Criação do index. e style (1.0.1) +style.css +links para os cards. +declaração de variavel global. =conecxão com git

---
+++ script.js
@@ -0,0 +1,18 @@
+const imageUrls = [
+    'Imgs/carta1.png',
+    'Imgs/carta2.png',
+    'Imgs/carta3.png',
+    'Imgs/carta4.png',
+    'Imgs/carta5.png',
+    'Imgs/carta6.png',
+    'Imgs/carta7.png',
+    'Imgs/carta8.png'
+]
+
+let cards = [];
+let flippedCards = [];
+let marchedPairs = 0;
+let attempts = 0;
+let gameStarted = false;
+let startTime = null;
+let timerInterval = null;
